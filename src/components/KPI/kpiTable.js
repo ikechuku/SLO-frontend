32e90304e10c997023ccddff06df2f79c283d69c
codeuiandy <frostandy41@gmail.com>
2020-05-06 03:38:34 +0100

    updates

--- src/components/KPI/kpiTable.js
+++ src/components/KPI/kpiTable.js
@@ -1,109 +1,59 @@
-import React, { Component } from 'react'
-import { httpGet,httpDelete } from '../../actions/data.action';
-import { hideLoader, showLoader } from '../../helpers/loader';
-import axios from 'axios'
+import React, { Component } from "react";
+import Table from "../../helpers/customTable";
 
-
-export default class KpiTable extends Component {
-
-    render() {
-        return (
-            <div>
-
-                <div class="table-responsive">
-												<table class="table table-bordered table-hover mb-0 text-nowrap">
-												<thead>
-													<tr>
-														<th>KPI</th>
-														<th>Responsibility</th>
-														<th>Job Roles</th>
-														<th>Weight Mark</th>
-                                                        <th>Actions</th>
-														
-													</tr>
-													</thead>
-												
-														<tbody>
-															
-													<tr>
-                                                    
-
-														<td>Deposits mobilized as in the budget</td>
-														<td>Savings mobilization</td>
-														<td>Head of Finance</td>
-                                                        <td>10</td>
-														<td>
-                                                            <span  data-toggle="modal" data-target="#exampleModal45" class='edit'>Edit</span>
-															<button className="del"
-															>delete</button>
-                                                        </td>
-													</tr>
-
-                                                    <tr>
-                                                    
-
-                                                    <td>Deposits mobilized as in the budget</td>
-                                                    <td>Savings mobilization</td>
-                                                    <td>Head of Finance</td>
-                                                    <td>10</td>
-                                                    <td>
-                                                        <span  data-toggle="modal" data-target="#exampleModal45" class='edit'>Edit</span>
-                                                        <button className="del"
-                                                        >delete</button>
-                                                    </td>
-                                                </tr>
-
-
-                                                <tr>
-                                                    
-
-                                                    <td>Deposits mobilized as in the budget</td>
-                                                    <td>Savings mobilization</td>
-                                                    <td>Head of Finance</td>
-                                                    <td>10</td>
-                                                    <td>
-                                                        <span  data-toggle="modal" data-target="#exampleModal45" class='edit'>Edit</span>
-                                                        <button className="del"
-                                                        >delete</button>
-                                                    </td>
-                                                </tr>
-
-
-                                                <tr>
-                                                    
-
-                                                    <td>Deposits mobilized as in the budget</td>
-                                                    <td>Savings mobilization</td>
-                                                    <td>Head of Finance</td>
-                                                    <td>10</td>
-                                                    <td>
-                                                        <span  data-toggle="modal" data-target="#exampleModal45" class='edit'>Edit</span>
-                                                        <button className="del"
-                                                        >delete</button>
-                                                    </td>
-                                                </tr>
-
-                                                <tr>
-                                                    
-
-                                                    <td>Deposits mobilized as in the budget</td>
-                                                    <td>Savings mobilization</td>
-                                                    <td>Head of Finance</td>
-                                                    <td>10</td>
-                                                    <td>
-                                                        <span  data-toggle="modal" data-target="#exampleModal45" class='edit'>Edit</span>
-                                                        <button className="del"
-                                                        >delete</button>
-                                                    </td>
-                                                </tr>
-													</tbody>
-												
-												
-												</table>
-											
-											</div>
-									
-            </div>
-        )
-    }
+class RoleTable extends Component {
+	constructor(props) {
+		super(props);
+	}
+	bodyRow = () => {
+		const body = this.props.roles.map((data, index) => ({
+			KPI: data.name,
+			Weight: data.weightMark,
+			action: (
+				<a>
+					<span
+						class="edit"
+						data-toggle="modal"
+						data-target="#roleModal"
+						onClick={() => this.props.handleEdit(data.id)}
+					>
+						Edit
+					</span>
+					<span class="del" onClick={() => this.props.handleDelete(data.id)}>
+						Delete
+					</span>
+				</a>
+			),
+		}));
+		return body;
+	};
+	header = () => {
+		const header = [
+			{
+				title: "KPI (filterable)",
+				prop: "KPI",
+				sortable: true,
+				filterable: true,
+			},
+			{ title: "Responsibility", prop: "Responsibility", sortable: true },
+			{ title: "Job Roles", prop: "JobRoles", sortable: true },
+			{ title: "Weight Mark", prop: "Weight" },
+			{ title: "Actions", prop: "action" },
+		];
+		return header;
+	};
+	render() {
+		return (
+			<div class="table-responsive" style={{ overflow: "hidden" }}>
+				<Table
+					body={this.bodyRow}
+					head={this.header}
+					rowsPerPage={10}
+					rowsPerPageOption={[10, 15, 20, 25]}
+				/>
+			</div>
+		);
+	}
 }
+
+export default RoleTable;
