ad75ebba59e51077783a6964ecdd8bf5356f6f4b
codeuiandy <frostandy41@gmail.com>
2020-05-28 00:34:59 +0100

    changes made

--- src/components/personalinfo/info.js
+++ src/components/personalinfo/info.js
@@ -23,77 +23,79 @@ export default class info extends Component {
 	render() {
 		return (
 			<Layout>
-				<section className="appheaderr5">
-					<h1>PERSONAL INFORMATION</h1>
-				</section>
-				<div className="personalInfo">
-					<div className="infoGrid1">
-						<img src={UserLogo} alt="Profile Pic" />
-						<span className="files675">Files</span>
-						<span>
-							<i class="fas fa-link"></i> Drivers License{" "}
-						</span>
-						<span>
-							<i class="fas fa-link"></i> Int’l Passport
-						</span>
-					</div>
-
-					<div className="infoGrid2">
-						<div className="userDetails">
-							<span>Full Name</span>
-							<span> Okeke Andrew</span>
-						</div>
-
-						<div className="userDetails">
-							<span>Date Of Birth</span>
-							<span> 21 jan 2025 </span>
-						</div>
-
-						<div className="userDetails">
-							<span>Marital Status</span>
-							<span> Single</span>
-						</div>
-
-						<div className="userDetails">
-							<span>Address</span>
-							<span>Uselu(Benin, Esan South-East, Edo, Nigeria)</span>
-						</div>
-
-						<div className="userDetails">
-							<span>Email</span>
-							<span>Codeuiandy@gmail.com</span>
-						</div>
-					</div>
-					<div className="infoGrid3">
-						<div className="userDetails">
-							<span>State of Origin</span>
-							<span>Edo State</span>
-						</div>
-
-						<div className="userDetails">
-							<span>Gender</span>
-							<span> Male </span>
-						</div>
-
-						<div className="userDetails">
-							<span>Nationality</span>
-							<span> Nigeria</span>
-						</div>
-
-						<div className="userDetails">
-							<span>Phone Numbers</span>
+				<div>
+					<section className="appheaderr5">
+						<h1>PERSONAL INFORMATION</h1>
+					</section>
+					<div className="personalInfo">
+						<div className="infoGrid1">
+							<img src={UserLogo} alt="Profile Pic" />
+							<span className="files675">Files</span>
 							<span>
-								<span></span>
-								<div style={{ display: "block" }}>0907786559</div>
-								<div>0907786559</div>
-								<span></span>
+								<i class="fas fa-link"></i> Drivers License{" "}
+							</span>
+							<span>
+								<i class="fas fa-link"></i> Int’l Passport
 							</span>
 						</div>
+
+						<div className="infoGrid2">
+							<div className="userDetails">
+								<span>Full Name</span>
+								<span> Okeke Andrew</span>
+							</div>
+
+							<div className="userDetails">
+								<span>Date Of Birth</span>
+								<span> 21 jan 2025 </span>
+							</div>
+
+							<div className="userDetails">
+								<span>Marital Status</span>
+								<span> Single</span>
+							</div>
+
+							<div className="userDetails">
+								<span>Address</span>
+								<span>Uselu(Benin, Esan South-East, Edo, Nigeria)</span>
+							</div>
+
+							<div className="userDetails">
+								<span>Email</span>
+								<span>Codeuiandy@gmail.com</span>
+							</div>
+						</div>
+						<div className="infoGrid3">
+							<div className="userDetails">
+								<span>State of Origin</span>
+								<span>Edo State</span>
+							</div>
+
+							<div className="userDetails">
+								<span>Gender</span>
+								<span> Male </span>
+							</div>
+
+							<div className="userDetails">
+								<span>Nationality</span>
+								<span> Nigeria</span>
+							</div>
+
+							<div className="userDetails">
+								<span>Phone Numbers</span>
+								<span>
+									<span></span>
+									<div style={{ display: "block" }}>0907786559</div>
+									<div>0907786559</div>
+									<span></span>
+								</span>
+							</div>
+						</div>
 					</div>
+					<EducationQ />
+					<EmploymentHistory />
+					<Garantor />
 				</div>
-				<EducationQ />
-				<EmploymentHistory />
-				<Garantor />
 			</Layout>
 		);
 	}
