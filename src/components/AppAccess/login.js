cafacaccf8bcc6b1d7eb3e1a2a459e30af031b0e
codeuiandy <frostandy41@gmail.com>
2020-05-27 12:24:27 +0100

    user login form

---
+++ src/components/AppAccess/login.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./login.css";
+
+export default class form extends Component {
+	render() {
+		return (
+			<div class="container-center">
+				<h1 className="appName">SLO </h1>
+
+				<form className="loginForm99">
+					<div class="container">
+						<div className="formHeader">
+							<h1>LOGIN</h1>
+						</div>
+
+						<input
+							className="logInInput9"
+							type="text"
+							placeholder="Enter Email"
+							required
+						/>
+
+						<input
+							className="logInInput9"
+							type="password"
+							placeholder="Enter Password"
+							required
+						/>
+
+						<button className="logInInput9button" type="submit">
+							Login To Your Account
+						</button>
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
