c3775ce7ccec40ebfb9dea8ae5c5837babd17944
Tijani Oluwafemi <tjhakeemus@gmail.com>
2020-08-29 07:38:00 +0100

    fix bug

--- src/App.js
+++ src/App.js
@@ -137,7 +137,7 @@ function App() {
 					<Route path="/loan-requests" component={LoanRequests} />
 					<Route path="/user-loan-request" component={UserLoan} />
 					<Route path="/salary_structure_items/:id" component={SalaryStructureItems}/>
-					<Route path="/process_payroll/:id" component={ProcessPayroll}/>
+					<Route path="/process_payroll/:id" component={PendingPayroll}/>
 					<Route path="/use_payslip/:id/:id2/:staffId" component={Payslip}/>
 					<Route path="/view_payroll/:id" component={ViewPayroll}/>
 					<Route path="/process_pending_payroll/:id" component={PendingPayroll}/>
