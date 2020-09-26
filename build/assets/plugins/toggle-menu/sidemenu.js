1825982c7a16184a2172fe0a147b196c92cda2e8
Tijani Oluwafemi <tjhakeemus@gmail.com>
2020-09-26 23:25:48 +0100

    fix base url and add build

---
+++ build/assets/plugins/toggle-menu/sidemenu.js
@@ -0,0 +1,44 @@
+(function () {
+	"use strict";
+
+	var slideMenu = $('.side-menu');
+	$('.app').addClass('sidebar-mini');
+	
+	// Toggle Sidebar
+	$(document).on("click", "[data-toggle='sidebar']", function(event) {
+		event.preventDefault();
+		$('.app').toggleClass('sidenav-toggled');
+	});
+	 
+	if ( $(window).width() > 739) {     
+		$('.app-sidebar').on("mouseover", function(event) {
+			event.preventDefault();
+			$('.app').removeClass('sidenav-toggled');
+		});
+	}
+	
+	// Activate sidebar slide toggle
+	$(document).on("click", "[data-toggle='slide']", function(event) {
+		event.preventDefault();
+		if(!$(this).parent().hasClass('is-expanded')) {
+			slideMenu.find("[data-toggle='slide']").parent().removeClass('is-expanded');
+		}
+		$(this).parent().toggleClass('is-expanded');
+	});
+
+	$(document).on("click", "[data-toggle='second-slide']", function(event) {
+		event.preventDefault();
+		if(!$(this).parent().hasClass('is-open')) {
+			slideMenu.find("[data-toggle='second-slide']").parent().removeClass('is-open');
+		}
+		$(this).parent().toggleClass('is-open');
+	});
+
+	// Set initial active toggle
+	$("[data-toggle='slide.'].is-expanded").parent().toggleClass('is-expanded');
+	$("[data-toggle='second-slide.'].is-open").parent().toggleClass('is-open');
+
+	//Activate bootstrip tooltips
+	$("[data-toggle='tooltip']").tooltip();
+
+})();
